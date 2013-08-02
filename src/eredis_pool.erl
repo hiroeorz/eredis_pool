df0cc02e3cfa8c8f0184bc556318a318c094f7bc
Atul Pundhir <atul.pundhir@gmail.com>
2013-08-02 17:03:30 +0900

    Added redis pipeline feature

--- src/eredis_pool.erl
+++ src/eredis_pool.erl
@@ -17,7 +17,7 @@
 
 %% API
 -export([start/0, stop/0]).
--export([q/2, q/3, transaction/2,
+-export([q/2, q/3, qp/2, qp/3, transaction/2,
          create_pool/2, create_pool/3, create_pool/4, create_pool/5,
          create_pool/6, create_pool/7, 
          delete_pool/1]).
@@ -116,6 +116,16 @@ q(PoolName, Command, Timeout) ->
     poolboy:transaction(PoolName, fun(Worker) ->
                                           eredis:q(Worker, Command, Timeout)
                                   end).
+
+
+qp(PoolName, Pipeline) ->
+    qp(PoolName, Pipeline, ?TIMEOUT).
+
+qp(PoolName, Pipeline, Timeout) ->
+    poolboy:transaction(PoolName, fun(Worker) ->
+   		eredis:q(Worker, Pipeline, Timeout)
+    end).
+
 
 transaction(PoolName, Fun) when is_function(Fun) ->
     F = fun(C) ->
